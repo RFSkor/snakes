1ce55691eb5a2987f44058ea4fed6f5d566e07e4
Your Name <you@example.com>
2025-04-27 16:38:19 +0600

    fix typos

--- script.js
+++ script.js
@@ -4,23 +4,23 @@ function getRandomInt(mix, max){
 }
 
 // Змея
-var convas = document.getElementsById('game')
+var canvas = document.getElementsById('game')
 var context = canvas.getContext("2d")
 var grid = 16
 // скорость змеи
 var count = 0
-var skane{
-    x: 160;
-    y: 160;
-    dx: grid;
-    dy: 0;
-    cells: [];
+var snake {
+    x: 160,
+    y: 160,
+    dx: grid,
+    dy: 0,
+    cells: [],
     maxCells: 4
 }
 
 // apple
 var apple = {
-    x: getRandomInt(0, 25) * grid
+    x: getRandomInt(0, 25) * grid,
     y: getRandomInt(0, 25) * grid
 }
 
