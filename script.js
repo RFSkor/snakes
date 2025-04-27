a7609e79cdd56797571b0d7c232338ee9e2683b2
RFSkor <rfskor@gmail.com>
2025-04-27 17:16:46 +0600

    fix js loads before html

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ function getRandomInt(mix, max){
 }
 
 // Змея
-var canvas = document.getElementsById('game')
-var context = canvas.getContext("2d")
+var canvas = document.getElementById('game')
+var context = canvas.getContext('2d');
 var grid = 16
 // скорость змеи
 var count = 0
-var snake {
+var snake = {
     x: 160,
     y: 160,
     dx: grid,
@@ -20,8 +20,8 @@ var snake {
 
 // apple
 var apple = {
-    x: getRandomInt(0, 25) * grid,
-    y: getRandomInt(0, 25) * grid
+    x: 320,
+    y: 320
 }
 
 function loop(){
@@ -29,6 +29,9 @@ function loop(){
     if (++count < 4){
         return
     }
+
+
+
 }
 
 // обнуляем скорость и поле
@@ -44,4 +47,20 @@ if (skake.cells.length > snake.maxCells){
     skake.cells.pop()
 }
 
+context.fillStyle = 'red';
+context.fillRect(apple.x, apple,y, grid - 1, grid - 1)
 
+context.fillStyle = 'green';
+context.fillRect(cell.x, cell.y, grid - 1, gird - 1)
+
+snake.x = 160;
+snake.x = 160;
+snake.cell =[];
+snake.maxCells = 4;
+skane.dx = grid;
+snake.dy = 0
+
+apple.x = getRandomInt(0, 25) * grid;
+apple.y = getRandomInt(0, 25) * grid;
+
+requestAnimationFrame(loop);
