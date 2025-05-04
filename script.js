3c0ee8ec59aa83242f3cf186fac48056e88ceabf
RFSkor <rfskor@gmail.com>
2025-05-04 16:33:03 +0600

    move the object in loop

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ var context = canvas.getContext('2d');
 var grid = 16
 // скорость змеи
 var count = 0
+
 var snake = {
     x: 160,
     y: 160,
@@ -16,44 +17,42 @@ var snake = {
     dy: 0,
     cells: [],
     maxCells: 4
-}
+};
 
 // apple
 var apple = {
     x: 320,
     y: 320
-}
+};
 
 function loop(){
     requestAnimationFrame(loop);
     if (++count < 4){
-        return
+        return;
     }
 
+    // обнуляем скорость и поле
+    count = 0;
+    context.clearRect(0, 0 , canvas.width, canvas.height);
 
+    // заставляем змейку двигаться
+    snake.y = snake.dy
+    snake.x = snake.dx
 
+    snake.cells.unshift({X: snake.x, y: snake.y})
+    if (snake.cells.length > snake.maxCells){
+        snake.cells.pop()
+    }
+
+    context.fillStyle = 'red';
+    context.fillRect(apple.x, apple.y, grid - 1, grid - 1)
+
+    snake.cells.forEach(function (cell, index) {
+        context.fillStyle = 'green';
+        context.fillRect(cell.x, cell.y, grid - 1, grid - 1);
+    });
 }
 
-// обнуляем скорость и поле
-count = 0;
-context.clearRect(0, 0 , canvas.width, canvas.height);
-
-// заставляем змейку двигаться
-snake.y = snake.dy
-snake.x = snake.dx
-
-snake.cells.unshift({X: snake.x, y: snake.y})
-if (snake.cells.length > snake.maxCells){
-    snake.cells.pop()
-}
-
-context.fillStyle = 'red';
-context.fillRect(apple.x, apple.y, grid - 1, grid - 1)
-
-snake.cells.forEach(function (cell, index) {
-    context.fillStyle = 'green';
-    context.fillRect(cell.x, cell.y, grid - 1, grid - 1)
-});
 snake.x = 160;
 snake.x = 160;
 snake.cell =[];
